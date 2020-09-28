13eefd76a48471b598c090e308001af68f9e44d5
Garner Lee <1672745+lgarner@users.noreply.github.com>
2020-09-28 14:46:54 -0700

    EdgeEventsListener with Guava

---
+++ EmptyMatchEngineApp/matchingengine/src/main/java/com/mobiledgex/matchingengine/EdgeEventsListener.java
@@ -0,0 +1,24 @@
+package com.mobiledgex.matchingengine;
+
+import com.google.common.eventbus.DeadEvent;
+import com.google.common.eventbus.Subscribe;
+
+import java.util.Map;
+import java.util.Set;
+
+import distributed_match_engine.AppClient;
+
+public class EdgeEvents {
+    @Subscribe
+    public void ClientEdgeEventReceiver(AppClient.ClientEdgeEvent cee) {
+        // Do stuff.
+        Map<String, String> tm = cee.getTagsMap();
+        String count = tm.get("count"); // Should be an internal HashMap with a Map interface.
+        System.out.println("Count from server: " + count);
+    }
+
+    @Subscribe
+    public void handleDeadEvent(DeadEvent deadEvent) {
+        System.out.println("You have messages, but are not subscribed to events.");
+    }
+}
