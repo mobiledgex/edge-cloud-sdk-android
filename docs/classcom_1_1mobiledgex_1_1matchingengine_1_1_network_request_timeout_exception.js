463829813f5a2a1f246b6ab3e55f3bf105f784fb
Garner Lee <1672745+lgarner@users.noreply.github.com>
2022-03-02 13:18:53 -0800

    Docs update.

--- docs/classcom_1_1mobiledgex_1_1matchingengine_1_1_network_request_timeout_exception.js
+++
@@ -1,4 +0,0 @@
-var classcom_1_1mobiledgex_1_1matchingengine_1_1_network_request_timeout_exception =
-[
-    [ "NetworkRequestTimeoutException", "classcom_1_1mobiledgex_1_1matchingengine_1_1_network_request_timeout_exception.html#a1f8c7529cc32b155dd4a427c394e9446", null ]
-];
--- docs/enumcom_1_1mobiledgex_1_1matchingengine_1_1_matching_engine_1_1_find_cloudlet_mode.js
+++
@@ -1,5 +0,0 @@
-var enumcom_1_1mobiledgex_1_1matchingengine_1_1_matching_engine_1_1_find_cloudlet_mode =
-[
-    [ "PERFORMANCE", "enumcom_1_1mobiledgex_1_1matchingengine_1_1_matching_engine_1_1_find_cloudlet_mode.html#ae62383eed1c319b42099b0deb52332a9", null ],
-    [ "PROXIMITY", "enumcom_1_1mobiledgex_1_1matchingengine_1_1_matching_engine_1_1_find_cloudlet_mode.html#aa8c2a581dd8116d4fb68eda4c7bf9abc", null ]
-];
